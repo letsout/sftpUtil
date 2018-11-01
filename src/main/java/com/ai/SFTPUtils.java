ed2b0284320777cbe9d4818de79d7759b1b83e33
godbless <ouht@asiainfo.com>
2018-11-01 17:50:26 +0800

    完成功能

--- src/main/java/com/ai/SFTPUtils.java
+++ src/main/java/com/ai/SFTPUtils.java
@@ -1,12 +1,8 @@
 package com.ai;
 
 import com.jcraft.jsch.*;
-import com.jcraft.jsch.ChannelSftp.LsEntry;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Properties;
 import java.util.Vector;
 
@@ -15,194 +11,195 @@ import java.util.Vector;
  * @Date 2018/10/26 10:03
  * @Desc
  **/
-public  class SFTPUtils {
+public class SFTPUtils {
 
-        private static ChannelSftp sftp;
+    private static ChannelSftp sftp;
 
-        private static SFTPUtils instance = null;
+    private static SFTPUtils instance = null;
 
-        private SFTPUtils() {
-        }
+    private static String host = "172.17.76.52";
+    private static int port = 22;
+    private static String username = "sc_13000";
+    private static String password = "iopsIOPS1234!@#$iops30";
 
-        public static SFTPUtils getInstance(String host,int port,String username,String password) {
+    private SFTPUtils() {
+    }
+
+    private static SFTPUtils getInstance() {
+        if (instance == null) {
             if (instance == null) {
-                if (instance == null) {
-                    instance = new SFTPUtils();
-                    //获取连接
-                    sftp = instance.connect(host, port, username, password);
-                }
-            }
-            return instance;
-        }
-
-        /**
-         * 连接sftp服务器
-         *
-         * @param host     主机
-         * @param port     端口
-         * @param username 用户名
-         * @param password 密码
-         * @return
-         */
-        public ChannelSftp connect(String host, int port, String username, String password) {
-            ChannelSftp sftp = null;
-            try {
-                JSch jsch = new JSch();
-                jsch.getSession(username, host, port);
-                Session sshSession = jsch.getSession(username, host, port);
-                sshSession.setPassword(password);
-                Properties sshConfig = new Properties();
-                sshConfig.put("StrictHostKeyChecking", "no");
-                sshSession.setConfig(sshConfig);
-                sshSession.connect();
-                System.out.println("SFTP Session connected.");
-                Channel channel = sshSession.openChannel("sftp");
-                channel.connect();
-                sftp = (ChannelSftp) channel;
-                System.out.println("Connected to " + host);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            return sftp;
-        }
-
-        /**
-         * 上传文件
-         *
-         * @param remoteDirectory  上传的目录
-         * @param uploadFile 要上传的文件
-         * @param localDirectory 本地文件路径
-         */
-        public boolean upload(String remoteDirectory, String uploadFile, String localDirectory) {
-            try {
-                sftp.cd(remoteDirectory);
-
-                File file = new File(uploadFile);
-
-                FileInputStream fileInputStream = new FileInputStream(file);
-                sftp.put(fileInputStream, file.getName());
-                fileInputStream.close();
-                return true;
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                return false;
+                instance = new SFTPUtils();
+                //获取连接
+                sftp = instance.connect(host, port, username, password);
             }
         }
+        return instance;
+    }
 
-        /**
-         * 下载文件
-         *
-         * @param remoteDirectory    下载目录
-         * @param saveFile 下载的文件
-         * @param localDirectory     存在本地的路径
-         */
-        public File download(String localDirectory, String saveFile,String remoteDirectory) {
-            try {
-                sftp.cd(remoteDirectory);
-                Vector<LsEntry> files = null;
-                try {
-                    //查看文件列表
-                    files = listFiles(localDirectory);
-                } catch (SftpException e) {
-                    e.printStackTrace();
-                }
-                for (LsEntry file : files) {
-                    System.out.println("###\t" + file.getFilename());
-                }
-                File file = new File(saveFile);
-                FileOutputStream fileOutputStream = new FileOutputStream(file);
-                sftp.get(localDirectory, fileOutputStream);
-                fileOutputStream.close();
-                return file;
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                return null;
+    /**
+     * 连接sftp服务器
+     *
+     * @param host     主机
+     * @param port     端口
+     * @param username 用户名
+     * @param password 密码
+     * @return
+     */
+    private ChannelSftp connect(String host, int port, String username, String password) {
+        ChannelSftp sftp = null;
+        try {
+            JSch jsch = new JSch();
+            jsch.getSession(username, host, port);
+            Session sshSession = jsch.getSession(username, host, port);
+            sshSession.setPassword(password);
+            Properties sshConfig = new Properties();
+            sshConfig.put("StrictHostKeyChecking", "no");
+            sshSession.setConfig(sshConfig);
+            sshSession.connect();
+            System.out.println("SFTP Session connected.");
+
+            Channel channel = sshSession.openChannel("sftp");
+            channel.connect();
+
+            sftp = (ChannelSftp) channel;
+            System.out.println("Connected to " + host);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return sftp;
+    }
+
+
+    /**
+     * @param remoteFile 远程文件全路径,路径不能以'/'结尾
+     * @param localFile  本地文件全路径
+     * @return
+     */
+    private void upload(String remoteFile, String localFile) {
+        FileInputStream localFileStream = null;
+        try {
+            //远程文件全路径,路径不能以'/'结尾
+            if (remoteFile.endsWith("/")) {
+                remoteFile = remoteFile.substring(0, remoteFile.length() - 1);
             }
-        }
-
-        /**
-         * 下载文件
-         *
-         * @param downloadFilePath 下载的文件完整目录
-         * @param saveFile     存在本地的路径
-         */
-        public File download(String downloadFilePath, String saveFile) {
+            String remoteDir = remoteFile.substring(0, remoteFile.lastIndexOf("/"));
+            System.out.println("making dirs");
+            //创建目录
+            mkDir(remoteDir);
+            localFileStream = new FileInputStream(new File(localFile));
+            System.out.println("uploading....");
+            sftp.put(localFileStream, remoteFile);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
             try {
-                int i = downloadFilePath.lastIndexOf('/');
-                if (i == -1) {
-                    return null;
-                }
-                sftp.cd(downloadFilePath.substring(0, i));
-                File file = new File(saveFile);
-                FileOutputStream fileOutputStream = new FileOutputStream(file);
-                sftp.get(downloadFilePath.substring(i + 1), fileOutputStream);
-                fileOutputStream.close();
-                return file;
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                return null;
-            }
-        }
-
-        /**
-         * 删除文件
-         *
-         * @param directory  要删除文件所在目录
-         * @param deleteFile 要删除的文件
-         */
-        public void delete(String directory, String deleteFile) {
-            try {
-                sftp.cd(directory);
-                sftp.rm(deleteFile);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-
-        public void disconnect() {
-            try {
-                sftp.getSession().disconnect();
-            } catch (JSchException e) {
-                System.out.println(e.getMessage());
-            }
-            sftp.quit();
-            sftp.disconnect();
-        }
-
-        /**
-         * 列出目录下的文件
-         *
-         * @param directory 要列出的目录
-         * @throws SftpException
-         */
-        public Vector<LsEntry> listFiles(String directory) throws SftpException {
-            return sftp.ls(directory);
-        }
-
-        public static void main(String[] args) throws IOException {
-            String host = "172.17.76.52";
-            int port= 22;
-            String username= "sc_13000";
-            String password = "iopsIOPS1234!@#$iops30";
-            String localDirectory = args[0];
-            String remotePath = args[1];
-            SFTPUtils sf =SFTPUtils.getInstance(host,port,username,password);
-//            com.ai.SFTPUtils sf =com.ai.SFTPUtils.getInstance("180.169.129.52",18022,"iopsc2280","iopsc2280!123");
-
-            sf.download(remotePath,localDirectory);
-
-/*
-            Vector<LsEntry> files = null;
-            try {
-                //查看文件列表
-                files = sf.listFiles(localDirectory);
-            } catch (SftpException e) {
+                if (localFileStream != null)
+                    localFileStream.close();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-            for (LsEntry file : files) {
-                System.out.println("###\t" + file.getFilename());
-            }
-            sf.disconnect();*/
+            System.out.println("upload complete!");
+            disconnect();
         }
+    }
+
+    /**
+     * @param remotePath 远程路径
+     * @param localPath  本地路径,必须以'/'结尾
+     * @return
+     */
+    private void download(String remotePath, String localPath) {
+        try {
+            //本地路径,必须以'/'结尾
+            if (!localPath.endsWith("/")) {
+                localPath += "/";
+            }
+
+            Vector<ChannelSftp.LsEntry> files = sftp.ls(remotePath);
+            if (files.size() <= 2) {
+                System.out.println("该目录下没有文件");
+                return;
+            }
+            sftp.cd(remotePath);
+            for (ChannelSftp.LsEntry file : files) {
+                String fileName = file.getFilename();
+                if (fileName.equals(".") || fileName.equals("..")) {
+                    continue;
+                }
+                System.out.println(fileName + "is downloading.");
+                OutputStream os = new FileOutputStream(new File(localPath + fileName));
+                sftp.get(fileName, os);
+                os.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("download complete!");
+            disconnect();
+        }
+    }
+
+    private void disconnect() {
+        try {
+            sftp.getSession().disconnect();
+            sftp.quit();
+            sftp.disconnect();
+        } catch (JSchException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void mkDir(String pathname) throws Exception {
+        //分割路径,从根目录开始遍历
+        String[] strings = pathname.split("/");
+        StringBuffer dirPath = new StringBuffer("/");
+        for (String str : strings) {
+            if (str.length() == 0) {
+                continue;
+            }
+            dirPath.append(str + "/");
+            String dir = dirPath.toString();
+            //目录是否存在
+            if (isDirExit(dir)) {
+                sftp.cd(dir);
+            } else {
+                sftp.mkdir(dir);
+                sftp.cd(dir);
+            }
+        }
+    }
+
+    private boolean isDirExit(String dir) {
+        boolean isDirExistFlag = false;
+        try {
+            SftpATTRS sftpATTRS = sftp.lstat(dir);
+            isDirExistFlag = sftpATTRS.isDir();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return isDirExistFlag;
+    }
+
+    /**
+     * @param args 0:d(下载)/u(上传) 1:remotePath 2:localPath
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        if (args.length < 3)
+            System.out.println("please input 3 params");
+
+        String remotePath = args[1];
+        String localPath = args[2];
+
+        SFTPUtils sftp = SFTPUtils.getInstance();
+
+        if ("d".equals(args[0])) {
+            sftp.download(remotePath, localPath);
+        } else if ("u".equals(args[0])) {
+            sftp.upload(remotePath, localPath);
+        } else {
+            System.out.println("please choose the right operation!");
+        }
+    }
 }
 
